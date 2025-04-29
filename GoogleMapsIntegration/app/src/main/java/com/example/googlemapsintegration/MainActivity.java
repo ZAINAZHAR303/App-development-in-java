9fca9e7be9e1c8b2e5861d07d428d43ad2da7a53
zain azhar <142168421+ZAINAZHAR303@users.noreply.github.com>
2025-04-29 16:32:51 +0500

    add handler to access the intergreated map page

--- GoogleMapsIntegration/app/src/main/java/com/example/googlemapsintegration/MainActivity.java
+++ GoogleMapsIntegration/app/src/main/java/com/example/googlemapsintegration/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.googlemapsintegration;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +11,26 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.button);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        btn.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+            startActivity(intent);
+            finish();
+        });
     }
+
+
 }
