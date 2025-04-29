195689498494f8d93d86d1de7de3c80f75ff075d
zain azhar <142168421+ZAINAZHAR303@users.noreply.github.com>
2025-04-29 16:31:31 +0500

    implement the functionality of tip calculator

--- practical/app/src/main/java/com/example/myapplication/TipCalculator.java
+++ practical/app/src/main/java/com/example/myapplication/TipCalculator.java
@@ -1,6 +1,10 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +12,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.text.DecimalFormat;
+
 public class TipCalculator extends AppCompatActivity {
 
+    private EditText editTextBillAmount, editTextTipPercentage;
+    private Button buttonCalculate;
+    private TextView textviewTipAmount;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +29,30 @@ public class TipCalculator extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        editTextBillAmount = findViewById(R.id.editTextBillAmount);
+        editTextTipPercentage =findViewById(R.id.editTextTipPercentage);
+        buttonCalculate = findViewById(R.id.buttonCalculate);
+        textviewTipAmount = findViewById(R.id.textViewTipAmount);
+
+        buttonCalculate.setOnClickListener(new View.OnClickListener(){
+        @Override
+            public void onClick(View v){
+            calculateTip();
+        }
+
+        });
+
+
     }
+    private void calculateTip(){
+        double billAmount = Double.parseDouble(editTextBillAmount.getText().toString());
+        double tipPercentage = Double.parseDouble(editTextTipPercentage.getText().toString());
+
+        double tipAmount = (billAmount*tipPercentage)/100;
+        DecimalFormat decimalFormat = new DecimalFormat("#.00");
+        String formattedTipAmount = decimalFormat.format(tipAmount);
+        textviewTipAmount.setText("Tip Amount : $" + formattedTipAmount);
+    }
+
 }
