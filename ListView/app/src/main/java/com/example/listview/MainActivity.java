bdb71ba478385465576870797c37346c528ecf88
zain azhar <142168421+ZAINAZHAR303@users.noreply.github.com>
2025-05-03 15:58:49 +0500

    add the java logic to implement listview

--- ListView/app/src/main/java/com/example/listview/MainActivity.java
+++ ListView/app/src/main/java/com/example/listview/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.listview;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +13,42 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    ListView myListView;
+    ArrayList<String> arrFruits;
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        myListView = findViewById(R.id.myListView); // link to XML
+        arrFruits = new ArrayList<>();
+
+// Add data to the list
+        arrFruits.add("Apple");
+        arrFruits.add("Banana");
+        arrFruits.add("Cherry");
+
+// Create Adapter to connect data to ListView
+        adapter = new ArrayAdapter<>(getApplicationContext(),R.layout.simple_list_item_1,R.id.tv1, arrFruits);
+
+// Set adapter to ListView
+        myListView.setAdapter(adapter);
+
+        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // Example: Show toast
+                Toast.makeText(getApplicationContext(), "Clicked: " + arrFruits.get(position), Toast.LENGTH_SHORT).show();
+            }
         });
+
+
     }
 }
