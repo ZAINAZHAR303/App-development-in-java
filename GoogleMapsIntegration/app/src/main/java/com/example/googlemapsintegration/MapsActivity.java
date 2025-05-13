be1cc25d4e1c2307d30a5923fe9a27b6d38786d7
zain azhar <142168421+ZAINAZHAR303@users.noreply.github.com>
2025-05-13 11:06:38 +0500

    access camera and storage application in java

--- GoogleMapsIntegration/app/src/main/java/com/example/googlemapsintegration/MapsActivity.java
+++ GoogleMapsIntegration/app/src/main/java/com/example/googlemapsintegration/MapsActivity.java
@@ -1,6 +1,10 @@
 package com.example.googlemapsintegration;
+import android.Manifest;
 
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
+
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import com.example.googlemapsintegration.R;
@@ -13,12 +17,16 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+
     private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
@@ -28,12 +36,22 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
+
         mMap = googleMap;
 
         // Add a marker and move the camera
         LatLng lahore = new LatLng(31.5204, 74.3587);
         mMap.addMarker(new MarkerOptions().position(lahore).title("Marker in Lahore"));
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(lahore, 12));
+//        mMap = googleMap;
+//
+//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+//            mMap.setMyLocationEnabled(true);
+//        } else {
+//            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+//        }
+
+
     }
 }
 
