d3a7abbf92a1bfc39492b070d99e574e8c4946f0
zain azhar <142168421+ZAINAZHAR303@users.noreply.github.com>
2025-05-22 15:51:41 +0500

    change model fruit desgin

--- RecyclerView/app/src/main/java/com/example/recyclerview/ModelFruit.java
+++ RecyclerView/app/src/main/java/com/example/recyclerview/ModelFruit.java
@@ -4,8 +4,31 @@ public class ModelFruit {
     int image;
     String name;
 
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public ModelFruit(int image, String name) {
         this.image = image;
         this.name = name;
     }
 }
+//ModelFruit card1 = new ModelFruit();
+//card1.setName("te string");
+
+//
+//ModelFruit card2 = new ModelFruit(3333,"the text for card 2");
+//card1.image;
+//card2.imag
